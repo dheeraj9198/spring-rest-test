01f4715db8782d6da35b926618ce974d9de0e222
dheeraj <dheeraj.sachan@superprofs.com>
2015-11-18 17:04:46 +0530

    Initial commit

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dheeraj on 18/11/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
